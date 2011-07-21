d3b86ccdde12517fed404112972af8021539ff32
dogmatic69 <dogmatic69@gmail.com>
2011-07-21 19:35:36 +0100

    adding the initial release for the fb plugin

---
+++ config/releases/map.php
@@ -0,0 +1,6 @@
+<?php
+$map = array(
+	1 => array(
+		'000008_facebook' => 'R4e28713b93684dfca0d118876318cd70'),
+);
+?>
---
+++ config/schema/schema.php
@@ -0,0 +1,15 @@
+<?php 
+/* SVN FILE: $Id$ */
+/* Facebook schema generated on: 2011-07-21 19:07:35 : 1311273275*/
+class FacebookSchema extends CakeSchema {
+	var $name = 'Facebook';
+
+	function before($event = array()) {
+		return true;
+	}
+
+	function after($event = array()) {
+	}
+
+}
+?>
